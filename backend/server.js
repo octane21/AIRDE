b430d35c0e1d6fc9addb127e977bb808f8876274
Your Name <carlitamba8@gmail.com>
2026-06-20 21:05:12 +0700

    UPDATE SERVER

--- backend/server.js
+++ backend/server.js
@@ -77,6 +77,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Internal server error' });
 });
 
-app.listen(PORT, () => {
-  console.log(`AIRDE backend running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0'() => {
+  console.log(`AIRDE backend running on ${PORT}`);
 });
